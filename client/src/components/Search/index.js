a5ce1dc733f0e7f73f19ee53f5549532b808af98
Marina Hoffmann <marinasthoffmann@gmail.com>
2026-06-09 21:30:59 +0200

    feat: integrate axios to fetch books from api

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,7 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
-import { useState } from "react";
-import { books } from "./SearchData";
+import { useEffect, useState } from "react";
+import { getBooks } from "../../services/books";
 
 const SearchContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -44,6 +44,16 @@ const ResultContainer = styled.div`
 
 function Search() {
     const [searchedBooks, setsearchedBooks] = useState([]);
+    const [books, setBooks] = useState([]);    
+
+    async function fetchBooks() {
+        const APIBooks = await getBooks();
+        setBooks(APIBooks);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     return (
         <SearchContainer>
--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bookRouter = require('./routes/book');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.use('/books', bookRouter);
 
